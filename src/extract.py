018ee617b94933abf0e17e202ac0519daf1fe1b3
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-12-03 23:12:21 +0200

    added the code to extract data from the database

--- src/extract.py
+++ src/extract.py
@@ -1,1 +1,36 @@
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+# Load credentials:
+# Load credentials
+password = os.getenv("PASSWORD")
+db_name = os.getenv("DB_NAME")
+host = "127.0.0.1"
+port = "3306"
+user = "root"
+
+# Define the connection string
+db_url = f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}"
+
+
+try:
+    print(db_name)
+    engine = create_engine(db_url)
+    ## create the query:
+    query = "SELECT * FROM bank_marketing"
+
+    # This line triggers the connection attempt
+    data = pd.read_sql_query(query, engine)
+
+    # If connection succeeds:
+    data.to_csv("data/raw_data.csv", index=False)
+
+    
+except Exception as e:
+    print(f"Connection Failed! Error: {e}")
+
 
