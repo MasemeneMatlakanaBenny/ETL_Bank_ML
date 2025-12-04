bca8f6aa874a3c48583dcbe0fbe05cc911d5a596
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-12-04 22:43:11 +0200

    added data extraction for the pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,1 +1,30 @@
+from prefect import task,flow
 
+@task(
+  name="data extraction",
+  task_name="extraction",
+  description="Extract the data from the databases"
+)
+def data_extraction():
+  """
+  Extract the messy and raw data from the databases
+  """
+  import os
+  from dotenv import load_dotenv
+  
+  load_dotenv()
+  ##connect to the db:
+  
+  password = os.getenv("PASSWORD")
+  db_name = os.getenv("DB_NAME")
+  host = "127.0.0.1"
+  port = "3306"
+  user = "root"
+  
+  db_url = f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}"
+
+  ## write sql query or script
+  query = "SELECT * FROM bank_marketing"
+  data = pd.read_sql_query(query, engine)
+  
+  return data
