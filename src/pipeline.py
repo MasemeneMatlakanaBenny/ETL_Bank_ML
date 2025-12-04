0d87db9bf41f0d403ec5ae73ccd9df258e441930
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-12-04 22:47:04 +0200

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -28,3 +28,31 @@ def data_extraction():
   data = pd.read_sql_query(query, engine)
   
   return data
+
+
+@task(
+  name="transformation",
+  task_name="data_transformation",
+  description="Handling nulls and making sure that the data is clean and consistent"
+)
+def data_transformation(messy_data:pd.DataFrame)->pd.DataFrame:
+  """
+  Transform the messy data and make it ready for any data workflow or consumering
+  """
+  import pandas as pd
+  import numpy as np
+  from datetime import datetime
+
+  ## transform the data
+  df["subscribe"]=df["subscribe"].replace(["yes","no"],[1,0])
+  df["contact"]=df["contact"].fillna("no_phone")
+  df["education"]=df["education"].fillna("no_education")
+
+
+  ## add the primary key value and the datetime -> for loading purposes
+  arr_index=np.arange(1,len(df)+1)
+  current_time = datetime.now()
+  
+  df['user_id']=arr_index
+  df['datetime']=current_time
+  
